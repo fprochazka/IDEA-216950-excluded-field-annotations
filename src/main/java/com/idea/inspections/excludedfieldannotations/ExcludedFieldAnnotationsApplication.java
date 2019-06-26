68e6feddbba362fb8635ef2af83b5ce28a9aad51
Filip Procházka <filip@prochazka.su>
2019-06-26 21:59:39 +0200

    init

---
+++ src/main/java/com/idea/inspections/excludedfieldannotations/ExcludedFieldAnnotationsApplication.java
@@ -0,0 +1,15 @@
+package com.idea.inspections.excludedfieldannotations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcludedFieldAnnotationsApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ExcludedFieldAnnotationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/idea/inspections/excludedfieldannotations/ExcludedFieldAnnotationsApplicationTests.java
@@ -0,0 +1,10 @@
+package com.idea.inspections.excludedfieldannotations;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcludedFieldAnnotationsApplicationTests
+{
+
+
+}
